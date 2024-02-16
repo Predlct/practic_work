73470ead660d301ef4853a50354406c4530a4914
Predlct <p.redict@mail.ru>
2024-02-16 18:00:31 +0700

    done

--- task15.py
+++ task15.py
@@ -1,6 +1,6 @@
 
-dium = int(input('Сколько сантиметров?\n'))/2.54
-fut = int(dium)/12
+duim = int(input('Сколько сантиметров?\n'))/2.54
+fut = int(duim)/12
 yard = fut/3
 mile = yard/1760
-print(yard, 'ярдов\n', mile, 'мили\n', fut, 'футов\n', dium, 'дюймов\n')
+print(yard, 'ярдов\n', mile, 'мили\n', fut, 'футов\n', duim, 'дюймов\n')
