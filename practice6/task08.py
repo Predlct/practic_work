085ce76d1d77173341cf52f7ca552f3f87e2c1d9
Predlct <p.redict@mail.ru>
2024-03-23 13:45:14 +0700

    practice6

---
+++ practice6/task08.py
@@ -0,0 +1,12 @@
+number = int(input('Введите число:\n'))
+
+output_number = list(range(-1, 201))
+
+if number < 10:
+    print(output_number[number])
+elif number < 191:
+    print(str(output_number[(number-9)//2 + 10])[number % 2])
+else:
+    print(str(output_number[(number-188)//3 + 100])[((number - 188) % 3)])
+
+
