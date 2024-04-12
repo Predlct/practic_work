0a04423ce41d1c830826c1b015503998832553c6
Predlct <p.redict@mail.ru>
2024-04-12 18:08:12 +0700

    practice7

--- practice8/task08.py
+++ practice8/task08.py
@@ -1,2 +1,15 @@
 
-for i in range ()
+
+number = '123456789'
+number_two = '1111111111'
+
+
+
+for i in range(1, 9+1):
+    print(f'{number[:i]} * 8 + {i} = {int(number[:i]) * 8 + i}', end='\n')
+
+for i in range(1, 9+1):
+    print(f'{number[:i]} * 9 + {i + 1} = {number_two[:i + 1]}', end='\n')
+
+for i in range(1, 9+1):
+    print(f'{number_two[:i]} * {number_two[:i]} = {int(number_two[:i])*int(number_two[:i])}', end='\n')
