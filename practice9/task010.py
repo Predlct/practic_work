0b8b33075ccb0299f2cbf538bffdc7e4e86023f6
Predlct <p.redict@mail.ru>
2024-04-18 20:00:21 +0700

    practice9

--- practice9/task010.py
+++ practice9/task010.py
@@ -1,6 +1,14 @@
 
 number = int(input('Введите количество кубиков, не превыщающее 100:\n'))
 
-for i in range(1, int(number/2)):
-    if number - i > number/2:
+for i in range(1, number+1):
+    if number == i:
+        if i == 1 or i == 2:
+            print(1)
+        elif i == 3:
+            print(2)
+        else:
+            print(number-2)
+
+
 
--- practice9/task08.py
+++ practice9/task08.py
@@ -3,8 +3,8 @@ number = int(input('Enter natural number:\n'))
 
 quantity = 0
 
-for i in range(1, round(number**(1/2))+1+1):
-    for n in range(1, round(number**(1/2))+1+1):
+for i in range(1, round(number**(1/2))+1):
+    for n in range(1, round(number**(1/2))+1):
         if i**2 + n**2 == number:
             quantity += 1
 
